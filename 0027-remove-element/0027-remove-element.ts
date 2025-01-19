f5c1a35df72f8a182f7e5bf7de6860b0c74b7477
Noe Camacho <noecl1234@gmail.com>
2025-01-19 16:56:30 -0600

    Time: 1 ms (22.5%), Space: 51.9 MB (13.78%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,10 +1,11 @@
 function removeElement(nums: number[], val: number): number {
+    let index = 0;
     for(let i = 0; i < nums.length; i++) {
-        if (nums[i] === val) {
-            nums.splice(i, 1);
-            i--;
+        if (nums[i] != val) {
+            nums[index] = nums[i];
+            index++;
         }
     }
 
-    return nums.length;
+    return index;
 };
